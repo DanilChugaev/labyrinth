36ce3900c4c11264903679bb5f433c414ef45d4e
Danil Chugaev <dmchugaev@gmail.com>
2025-08-27 00:15:43 +0700

    adds preloader

--- src/main.ts
+++ src/main.ts
@@ -4,35 +4,60 @@ import { setupButton } from './elements/button.ts';
 import { setupSelect } from './elements/select.ts';
 import labyrinth from '/labyrinth.svg';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+const app = document.querySelector<HTMLDivElement>('#app')!;
+const styles = getComputedStyle(app);
+const figureColor = styles.getPropertyValue('--figure-color');
+
+app.innerHTML = `
   <div class="header">
     <img class="header__logo" src="${labyrinth}" alt="Labyrinth icon" width="50">
     <h1 class="header__title">Labyrinth</h1>
   </div>
   
   <div class="game">
-    <canvas id="canvas"></canvas>
+    <div class="game__canvas-container">
+      <svg id="preloader" class="game__preloader" width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="50" cy="50" r="40" fill="none" stroke="${figureColor}" stroke-width="8" stroke-linecap="round" stroke-dasharray="125.6 125.6">
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            from="0 50 50"
+            to="360 50 50"
+            dur="1s"
+            repeatCount="indefinite"
+          />
+        </circle>
+      </svg>
+      
+      <canvas id="canvas"></canvas>
+    </div>
     
     <div class="game__actions">
       <div class="game__select-container">
-        <select class="game__select" id="select"></select>
+        <select class="game__select" id="select" disabled></select>
       </div>
       
-      <button class="game__button" id="button" type="button">Новый лабиринт</button>
+      <button class="game__button" id="button" type="button" disabled>Новый лабиринт</button>
     </div>
   </div>
 `;
 
-const { drawLabyrinth, redrawLabyrinth } = setupCanvas(
-  document.querySelector<HTMLCanvasElement>('#canvas')!,
-);
-setupButton(document.querySelector<HTMLButtonElement>('#button')!, redrawLabyrinth);
-setupSelect(document.querySelector<HTMLSelectElement>('#select')!, redrawLabyrinth);
+const canvas = document.querySelector<HTMLCanvasElement>('#canvas')!;
+const button = document.querySelector<HTMLButtonElement>('#button')!;
+const select = document.querySelector<HTMLSelectElement>('#select')!;
+const preloader = document.querySelector<HTMLOrSVGImageElement>('#preloader')!;
 
-drawLabyrinth();
+const { drawLabyrinth, redrawLabyrinth } = setupCanvas(canvas);
+setupButton(button, redrawLabyrinth);
+setupSelect(select, redrawLabyrinth);
+
+await drawLabyrinth();
+
+button.disabled = false;
+select.disabled = false;
+preloader.style.display = 'none';
 
 // 1 - todo: оптимизировать код генерации лабиринта (мб на мобилках ограничить размер клеток)
-// 2 - todo: добавить какой-то лоадер, чтобы при выборе большого размера страница не висла
 // 3 - todo: добавить стрелки для перемещений
 // 4 - todo: добавить стартовую и конечную точки
 // 5 - todo: добавить перемещение по лабиринту
