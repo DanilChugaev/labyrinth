1e726301c0b95c534bd451ec1da6b3e6c04f6caa
Danil Chugaev <dmchugaev@gmail.com>
2026-01-16 21:08:11 +0700

    adds deploy 3

--- src/generator.ts
+++ src/generator.ts
@@ -1,7 +1,7 @@
 import { runWorker } from './worker/runWorker.ts';
 
 export async function generateLabyrinth(size: number): Promise<Uint8Array> {
-  const worker = new Worker('src/worker/worker.ts', { type: 'module' });
+  const worker = new Worker(new URL('./worker/worker.ts', import.meta.url), { type: 'module' });
 
   return await runWorker<number, Uint8Array>(worker, size);
 }
