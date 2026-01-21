eb1f71ff405423ac068735aaabd0dc2f273a85d7
Danil Chugaev <dmchugaev@gmail.com>
2026-01-21 22:01:24 +0700

    adds PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,32 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   base: '/labyrinth/',
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Labyrinth Game',
+        short_name: 'Labyrinth',
+        description: 'Procedural labyrinth game',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg}']
+      }
+    })
+  ]
 });
